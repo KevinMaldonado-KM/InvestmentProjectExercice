ad5895b7da05f626f05c6d48069a0cb0968afb5c
Kévin MALDONADO <kevin.maldonado@etu.univ-amu.fr>
2025-07-25 02:01:30 +0200

    Exercice 3 - Composant Investment Result permettant d'afficher les résultat de manière dynamique et normaliser grâce au pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,21 @@ import { Component } from '@angular/core';
 
 import { HeaderComponent } from "./header/header.component";
 import { UserInputComponent } from "./user-input/user-input.component";
+import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
 
 import { type InvestmentInput } from './investment-input.model';
+import { type InvestmentOuput } from './investment-result.model';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
 })
 export class AppComponent {
+  resultsData?: InvestmentOuput[];
+
   onCalculateInvestmentResults(data: InvestmentInput) {
   const {initialInvestment, duration, expectedReturn, annualInvestment} = data;
   const annualData = [];
@@ -33,7 +38,7 @@ export class AppComponent {
     });
   }
 
-  console.log(annualData)
+  this.resultsData = annualData;
 }
 
 }
